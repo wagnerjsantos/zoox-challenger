85cb37294e99fe6d19d0a6ea435c7b4df38935e4
Wagner Pereira <wagner_junior31@hotmail.com>
2024-05-28 22:11:35 -0300

    adicionando cors

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 8000
 const routes = require("./src/routes/index")
 const ConnectDatabase = require("./src/config/database")
 const ConnectionOptions = require("./src/config/typeorm")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 app.use(bodyParser.json())
+
+app.use(cors())
 
 ConnectDatabase(ConnectionOptions)
 
 app.use("/", routes)
 
 app.listen(port, () => {
-    console.log("rodando porta 3000")
+    console.log("rodando porta 8000")
 })
